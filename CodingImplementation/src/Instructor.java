cf5fb79ad8d1004fe1920af42f7557f6b29189a7
7anine <117124318+7anine@users.noreply.github.com>
2024-10-18 19:16:00 -0400

    Instructor Class created

---
+++ CodingImplementation/src/Instructor.java
@@ -0,0 +1,55 @@
+public class Instructor {
+    private String name;
+    private String specialization;
+    private String[] cities;
+    private String phone;
+    private boolean availability;
+
+    public Instructor(String name, String specialization, String[] cities, String phone, boolean availability) {
+        this.name = name;
+        this.specialization = specialization;
+        this.cities = cities;
+        this.phone = phone;
+        this.availability = availability;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public String[] getCities() {
+        return cities;
+    }
+
+    public void setCities(String[] cities) {
+        this.cities = cities;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean isAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
+}
