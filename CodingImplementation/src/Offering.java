dfdec5f39a167ed63147ccbe8f3af0096b5ecaa1
y-mouatif <117135148+y-mouatif@users.noreply.github.com>
2024-10-18 21:09:08 -0400

    Create Offering.java

---
+++ CodingImplementation/src/Offering.java
@@ -0,0 +1,50 @@
+public class Offering extends Record {
+    private String offeringId;
+    private String location;
+    private String lessonId;
+    private String instructorId;
+
+    // Constructor
+    public Offering(String offeringId, String location, String lessonId, String instructorId) {
+        super();  // Call to parent class constructor if necessary
+        this.offeringId = offeringId;
+        this.location = location;
+        this.lessonId = lessonId;
+        this.instructorId = instructorId;
+    }
+
+    // Accessors (Getters)
+    public String getOfferingId() {
+        return offeringId;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getLessonId() {
+        return lessonId;
+    }
+
+    public String getInstructorId() {
+        return instructorId;
+    }
+
+    // Mutators (Setters)
+    public void setOfferingId(String offeringId) {
+        this.offeringId = offeringId;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setLessonId(String lessonId) {
+        this.lessonId = lessonId;
+    }
+
+    public void setInstructorId(String instructorId) {
+        this.instructorId = instructorId;
+    }
+}
+ 
