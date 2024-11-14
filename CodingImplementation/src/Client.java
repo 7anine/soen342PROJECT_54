8be86bcce991dcaae46ae4d6ca5727b459d990f0
7anine <117124318+7anine@users.noreply.github.com>
2024-11-14 16:36:09 -0500

    Client: Concurrency added

--- CodingImplementation/src/Client.java
+++ CodingImplementation/src/Client.java
@@ -1,6 +1,8 @@
 
 import java.sql.*;
 import java.util.Scanner;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 import database.DatabaseConnection;
 
 public class Client extends Record {
@@ -12,6 +14,8 @@ public class Client extends Record {
     private int guardianID = 0;
 
     private int ID;
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    private static final ReentrantReadWriteLock staticLock = new ReentrantReadWriteLock();
 
     public Client(String name, int age, String email) {
         super();  // Call the parent class constructor if necessary
@@ -111,39 +115,48 @@ public class Client extends Record {
         this.isOver18 = client.isOver18();
         this.guardianID = client.getGuardianID();
 
-        // Insert into database
-        String insertQuery = "INSERT INTO Client (clientId, name, age, email, isOver18, guardianClientId, password) " +
-                "VALUES (?, ?, ?, ?, ?, ?, ?)";
+        lock.writeLock().lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + " is writing to the database from method createClientAccount" );
 
-        try (Connection connection = DatabaseConnection.getConnection();
-             PreparedStatement statement = connection.prepareStatement(insertQuery)) {
+            // Insert into database
+            String insertQuery = "INSERT INTO Client (clientId, name, age, email, isOver18, guardianClientId, password) " +
+                    "VALUES (?, ?, ?, ?, ?, ?, ?)";
 
-            statement.setInt(1, this.ID);
-            statement.setString(2, this.name);
-            statement.setInt(3, this.age);
-            statement.setString(4, this.email);
-            statement.setBoolean(5, this.isOver18);
-            if (this.getGuardianID()!= 0) {
-                statement.setInt(6, this.guardianID);
-            } else {
-                statement.setNull(6, java.sql.Types.INTEGER);
+            try (Connection connection = DatabaseConnection.getConnection();
+                 PreparedStatement statement = connection.prepareStatement(insertQuery)) {
+
+                statement.setInt(1, this.ID);
+                statement.setString(2, this.name);
+                statement.setInt(3, this.age);
+                statement.setString(4, this.email);
+                statement.setBoolean(5, this.isOver18);
+                if (this.getGuardianID()!= 0) {
+                    statement.setInt(6, this.guardianID);
+                } else {
+                    statement.setNull(6, java.sql.Types.INTEGER);
+                }
+                statement.setString(7, password);
+
+                int rowsAffected = statement.executeUpdate();
+                if (rowsAffected > 0) {
+                    System.out.println("Client registered with the following details:");
+                    System.out.println("Name: " + this.name);
+                    System.out.println("Age: " + this.age);
+                    System.out.println("Email: " + this.email);
+                    System.out.println("Is Over 18: " + this.isOver18);
+                    System.out.println("Guardian Client ID: " + (this.guardianID != 0 ? this.guardianID : 0));
+                } else {
+                    System.out.println("Failed to register the client.");
+                }
+
+            } catch (SQLException e) {
+                System.err.println("Error adding client to the database: " + e.getMessage());
             }
-            statement.setString(7, password);
 
-            int rowsAffected = statement.executeUpdate();
-            if (rowsAffected > 0) {
-                System.out.println("Client registered with the following details:");
-                System.out.println("Name: " + this.name);
-                System.out.println("Age: " + this.age);
-                System.out.println("Email: " + this.email);
-                System.out.println("Is Over 18: " + this.isOver18);
-                System.out.println("Guardian Client ID: " + (this.guardianID != 0 ? this.guardianID : 0));
-            } else {
-                System.out.println("Failed to register the client.");
-            }
-
-        } catch (SQLException e) {
-            System.err.println("Error adding client to the database: " + e.getMessage());
+        } finally {
+            lock.writeLock().unlock();  // Release the read lock
+            System.out.println(Thread.currentThread().getName() + " finished writing to the database from method createClientAccount");
         }
     }
 
@@ -163,46 +176,57 @@ public class Client extends Record {
 
         Client guardian = new Client(name, age, email);
 
-        // Insert guardian information into the database and retrieve the GuardianID
-        try (Connection connection = DatabaseConnection.getConnection()) {
-            String insertGuardianSQL = "INSERT INTO Client (name, age, email) VALUES (?, ?, ?)";
+        lock.writeLock().lock();
+        try {
+                System.out.println(Thread.currentThread().getName() + " is writing to the database from method requestGuardian" );
 
-            try (PreparedStatement statement = connection.prepareStatement(insertGuardianSQL, Statement.RETURN_GENERATED_KEYS)) {
-                statement.setString(1, name);
-                statement.setInt(2, age);
-                statement.setString(3, email);
-                statement.executeUpdate();
+                // Insert guardian information into the database and retrieve the GuardianID
+            try (Connection connection = DatabaseConnection.getConnection()) {
+                String insertGuardianSQL = "INSERT INTO Client (name, age, email) VALUES (?, ?, ?)";
 
-                // Retrieve the generated guardian ID
-                ResultSet generatedKeys = statement.getGeneratedKeys();
-                if (generatedKeys.next()) {
-                    int guardianID = generatedKeys.getInt(1);
-                    guardian.setID(guardianID); // Assuming Client class has a setID() method
-                    System.out.println("Id of the guardian of " + this.name + " is: " + guardianID);
-                    // Update current client with GuardianID
-                    String updateGuardianIDSQL = "UPDATE Client SET GuardianID = ? WHERE id = ?";
-                    try (PreparedStatement updateStatement = connection.prepareStatement(updateGuardianIDSQL)) {
-                        updateStatement.setInt(1, guardianID);
-                        updateStatement.setInt(2, this.ID); // Assuming current client’s ID is set
-                        updateStatement.executeUpdate();
+                try (PreparedStatement statement = connection.prepareStatement(insertGuardianSQL, Statement.RETURN_GENERATED_KEYS)) {
+                    statement.setString(1, name);
+                    statement.setInt(2, age);
+                    statement.setString(3, email);
+                    statement.executeUpdate();
+
+                    // Retrieve the generated guardian ID
+                    ResultSet generatedKeys = statement.getGeneratedKeys();
+                    if (generatedKeys.next()) {
+                        int guardianID = generatedKeys.getInt(1);
+                        guardian.setID(guardianID); // Assuming Client class has a setID() method
+                        System.out.println("Id of the guardian of " + this.name + " is: " + guardianID);
+                        // Update current client with GuardianID
+                        String updateGuardianIDSQL = "UPDATE Client SET GuardianID = ? WHERE id = ?";
+                        try (PreparedStatement updateStatement = connection.prepareStatement(updateGuardianIDSQL)) {
+                            updateStatement.setInt(1, guardianID);
+                            updateStatement.setInt(2, this.ID); // Assuming current client’s ID is set
+                            updateStatement.executeUpdate();
+                        }
+                    } else {
+                        System.out.println("Failed to retrieve Guardian ID.");
                     }
-                } else {
-                    System.out.println("Failed to retrieve Guardian ID.");
                 }
+            } catch (SQLException e) {
+                System.err.println("Database error: " + e.getMessage());
             }
-        } catch (SQLException e) {
-            System.err.println("Database error: " + e.getMessage());
+        } finally {
+            lock.writeLock().unlock();  // Release the read lock
+            System.out.println(Thread.currentThread().getName() + " finished writing to the database from method requestGuardian");
         }
-
         return guardian;
     }
 
 
 
     public static boolean validPassword(int clientID, int clientPassword) {
-        String selectQuery = "SELECT password FROM Client WHERE clientId = ?";
 
-        try (Connection connection = DatabaseConnection.getConnection();
+        staticLock.readLock().lock();  // Acquire the read lock
+        try {
+            System.out.println(Thread.currentThread().getName() + " is reading from database from method validPassword");
+            String selectQuery = "SELECT password FROM Client WHERE clientId = ?";
+
+            try (Connection connection = DatabaseConnection.getConnection();
              PreparedStatement statement = connection.prepareStatement(selectQuery)) {
 
             statement.setInt(1, clientID);
@@ -225,39 +249,51 @@ public class Client extends Record {
 
         } catch (SQLException e) {
             System.err.println("Error during client sign-in: " + e.getMessage());
+        }} finally {
+            staticLock.readLock().unlock();  // Release the read lock
+            System.out.println(Thread.currentThread().getName() + " finished reading from database from method validPassword.");
         }
 
         return false;
     }
     public static Client ClientSignIn(int clientId, int clientPassword) {
         if (validPassword(clientId, clientPassword)) {
-            String selectQuery = "SELECT clientId, name, age, email, isOver18, guardianClientId FROM Client WHERE clientId = ?";
 
-            try (Connection connection = DatabaseConnection.getConnection();
-                 PreparedStatement statement = connection.prepareStatement(selectQuery)) {
+            staticLock.readLock().lock();
+            try {
+                System.out.println(Thread.currentThread().getName() + " is reading from database from method clientSignIn");
 
-                statement.setInt(1, clientId);
+                    String selectQuery = "SELECT clientId, name, age, email, isOver18, guardianClientId FROM Client WHERE clientId = ?";
 
-                try (ResultSet resultSet = statement.executeQuery()) {
-                    if (resultSet.next()) {
-                        // Retrieve client data from the ResultSet
-                        int id = resultSet.getInt("clientId");
-                        String name = resultSet.getString("name");
-                        int age = resultSet.getInt("age");
-                        String email = resultSet.getString("email");
-                        boolean isOver18 = resultSet.getBoolean("isOver18");
-                        int guardianId = resultSet.getInt("guardianClientId");
-                        if (resultSet.wasNull()) {
-                            guardianId = 0; // or any other default value indicating no guardian
+                try (Connection connection = DatabaseConnection.getConnection();
+                     PreparedStatement statement = connection.prepareStatement(selectQuery)) {
+
+                    statement.setInt(1, clientId);
+
+                    try (ResultSet resultSet = statement.executeQuery()) {
+                        if (resultSet.next()) {
+                            // Retrieve client data from the ResultSet
+                            int id = resultSet.getInt("clientId");
+                            String name = resultSet.getString("name");
+                            int age = resultSet.getInt("age");
+                            String email = resultSet.getString("email");
+                            boolean isOver18 = resultSet.getBoolean("isOver18");
+                            int guardianId = resultSet.getInt("guardianClientId");
+                            if (resultSet.wasNull()) {
+                                guardianId = 0; // or any other default value indicating no guardian
+                            }
+
+                            // Create and return the Client object
+                            return new Client(name, age, email, id, guardianId);
                         }
-
-                        // Create and return the Client object
-                        return new Client(name, age, email, id, guardianId);
                     }
-                }
 
-            } catch (SQLException e) {
-                System.err.println("Error retrieving client data: " + e.getMessage());
+                } catch (SQLException e) {
+                    System.err.println("Error retrieving client data: " + e.getMessage());
+                }
+            } finally {
+                staticLock.readLock().unlock(); // Release the instance-level write lock
+                System.out.println(Thread.currentThread().getName() + " finished reading to the database from method clientSignIn");
             }
         } else {
             System.out.println("Sign-in failed. Cannot create client object.");
@@ -293,42 +329,50 @@ public class Client extends Record {
         String query = "SELECT * FROM Offering";
         Scanner scanner = new Scanner(System.in);
 
-        try (Connection connection = DatabaseConnection.getConnection();
-             PreparedStatement statement = connection.prepareStatement(query);
-             ResultSet resultSet = statement.executeQuery()) {
+        lock.readLock().lock();
+        try{
+            System.out.println(Thread.currentThread().getName() + " is reading from the database from method viewOfferings");
 
-            System.out.println("\n--- Available Offerings ---");
-            boolean offeringsFound = false;
+                try (Connection connection = DatabaseConnection.getConnection();
+                 PreparedStatement statement = connection.prepareStatement(query);
+                 ResultSet resultSet = statement.executeQuery()) {
 
-            while (resultSet.next()) {
-                int offeringId = resultSet.getInt("offeringId");
-                int lessonId = resultSet.getInt("lessonId");
-                int instructorId = resultSet.getInt("instructorId");
+                System.out.println("\n--- Available Offerings ---");
+                boolean offeringsFound = false;
 
-                System.out.println("Offering ID: " + offeringId);
-                System.out.println("Lesson ID: " + lessonId);
-                System.out.println("instructor ID: " + instructorId);
-                System.out.println("--------------------------");
+                while (resultSet.next()) {
+                    int offeringId = resultSet.getInt("offeringId");
+                    int lessonId = resultSet.getInt("lessonId");
+                    int instructorId = resultSet.getInt("instructorId");
 
-                offeringsFound = true;
+                    System.out.println("Offering ID: " + offeringId);
+                    System.out.println("Lesson ID: " + lessonId);
+                    System.out.println("instructor ID: " + instructorId);
+                    System.out.println("--------------------------");
+
+                    offeringsFound = true;
+                }
+
+                if (!offeringsFound) {
+                    System.out.println("No offerings available at the moment.");
+                    return;
+                }
+
+                System.out.print("Enter the Offering ID to create a new booking, or enter 0 to go back: ");
+                int selectedId = scanner.nextInt();
+
+                if (selectedId == 0) {
+                    return; // Go back to client portal menu
+                } else {
+                    handleOfferingSelection(selectedId);
+                }
+
+            } catch (SQLException e) {
+                System.err.println("Error fetching offerings: " + e.getMessage());
             }
-
-            if (!offeringsFound) {
-                System.out.println("No offerings available at the moment.");
-                return;
-            }
-
-            System.out.print("Enter the Offering ID to create a new booking, or enter 0 to go back: ");
-            int selectedId = scanner.nextInt();
-
-            if (selectedId == 0) {
-                return; // Go back to client portal menu
-            } else {
-                handleOfferingSelection(selectedId);
-            }
-
-        } catch (SQLException e) {
-            System.err.println("Error fetching offerings: " + e.getMessage());
+        } finally {
+            lock.readLock().unlock(); // Release the instance-level write lock
+            System.out.println(Thread.currentThread().getName() + " finished reading to the database from method viewOfferings");
         }
     }
 
@@ -344,7 +388,11 @@ public class Client extends Record {
     private void addBookingToDatabase(Booking booking) {
         String insertQuery = "INSERT INTO Booking (bookingId, clientId, offeringId) VALUES (?, ?, ?)";
 
-        try (Connection connection = DatabaseConnection.getConnection();
+        lock.writeLock().lock();
+        try{
+            System.out.println(Thread.currentThread().getName() + " is writing to the database from method addBookingtoDatabase");
+
+            try (Connection connection = DatabaseConnection.getConnection();
              PreparedStatement statement = connection.prepareStatement(insertQuery)) {
 
             // Set values for the bookingId, clientId, and lessonId columns
@@ -361,6 +409,10 @@ public class Client extends Record {
 
         } catch (SQLException e) {
             System.err.println("Error creating booking: " + e.getMessage());
+        }
+        } finally {
+            lock.writeLock().unlock(); // Release the instance-level write lock
+            System.out.println(Thread.currentThread().getName() + " finished writing to the database from method adBookingtoDatabase");
         }
     }
 }
